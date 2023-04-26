ce9cced9f3a77748a0aa3447df20bd30c7fba55e
circuspony <endofrever@gmail.com>
2023-04-27 01:46:15 +0700

    feat: expert by clusters

--- components/editor/fcEditor.tsx
+++ components/editor/fcEditor.tsx
@@ -6,6 +6,7 @@ import IndicatorAnalysis from "./modes/indicatorAnalysis"
 import RegressionMode from "./modes/regressionMode"
 import ClusterComparison from "./modes/clusterComparison"
 import AccumulatorMode from "./modes/accumulatorMode"
+import ExpertMode from "./modes/expertMode"
 import { fchmod } from "fs"
 
 const METHODS = {
@@ -31,12 +32,14 @@ const FactorConnectionEditor = ({
     changeFactorConnectionInfluence,
     setFactorConnectionData
 }) => {
+    const [method, setMethod] = useState(null);
+    const [currentEval, setCurrentEval] = useState([]);
 
     const getFactorById = (id) => {
         return factorData.filter(f => f !== null).find(f => f.id === id)
     }
     const updateExpertEvals = () => {
-        if (getFactorById(factorConnectionData[currentFСEditor].start).isExternal) {
+        if (getFactorById(factorConnectionData[currentFСEditor].start)?.labels) {
             let expertEvals = new Array(factorEvals.find(fe => fe.id === getFactorById(factorConnectionData[currentFСEditor].start).id).eLabels.length).fill(0)
             setCurrentEval(expertEvals)
         }
@@ -48,6 +51,7 @@ const FactorConnectionEditor = ({
         setMethod(null)
         setCurrentEval([])
     }, [currentFСEditor])
+
     const trace = (fe) => {
         let wayFound = false
         let ways = [[factorConnectionData[currentFСEditor].start, factorConnectionData[currentFСEditor].end]]
@@ -68,8 +72,7 @@ const FactorConnectionEditor = ({
         return ways
 
     }
-    const [method, setMethod] = useState(null);
-    const [currentEval, setCurrentEval] = useState([]);
+
 
     const setEval = (ev) => {
         changeFactorConnectionInfluence(currentFСEditor, "?", ev)
@@ -197,68 +200,14 @@ const FactorConnectionEditor = ({
                             />
                         </> : null}
                         {method === METHODS.EXPERT ?
-                            <div className="flex-col mt-4">
-                                {getFactorById(factorConnectionData[currentFСEditor].start).isExternal ?
-                                    <>
-                                        {
-                                            factorEvals.findIndex(fe => fe.id === getFactorById(factorConnectionData[currentFСEditor].start).id) >= 0 ?
-                                                <>
-                                                    <div className="text-lg mt-4 font-bold mb-1">Ввести оценку</div>
-                                                    {
-                                                        factorEvals.find(fe => fe.id === getFactorById(factorConnectionData[currentFСEditor].start).id).eLabels.map((fe, fi) =>
-                                                            <>
-                                                                <div>{"Кластер " + fe}</div>
-                                                                <input
-                                                                    value={currentEval[fi]}
-                                                                    onChange={(e) => {
-                                                                        setCurrentEval(currentEval.map((ce, ci) => {
-                                                                            if (ci === fi) {
-                                                                                return e.target.value
-                                                                            }
-                                                                            return ce
-                                                                        }))
-
-                                                                    }}
-                                                                    style={{
-                                                                        outline: "none",
-                                                                    }}
-                                                                    className={`text-xl p-1 w-20 border-2 border-violet-border rounded-xl bg-violet`}
-                                                                />
-                                                            </>)
-
-                                                    }
-                                                </> :
-                                                <div>Не хватает данных для расчета</div>
-                                        }
-                                    </> :
-                                    <>
-                                        <div className="text-lg mt-4 font-bold mb-1">Ввести оценку</div>
-                                        <input
-                                            value={currentEval[0]}
-                                            onChange={(e) => {
-                                                setCurrentEval(currentEval.map((ce, ci) => {
-                                                    if (ci === 0) {
-                                                        return parseFloat(e.target.value)
-                                                    }
-                                                    return ce
-                                                }))
-
-                                            }}
-                                            style={{
-                                                outline: "none",
-                                            }}
-                                            className={`text-xl p-1 w-20 border-2 border-violet-border rounded-xl bg-violet`}
-                                        />
-                                    </>}
-
-                                <div
-                                    onClick={() => {
-                                        setEval(currentEval)
-                                    }}
-                                    className={`h-16 mt-2  w-20 justify-center relative noselect z-40 transition-all duration-300 items-center flex w-full cursor-pointer text-white font-medium bg-violet-border border-2 border-violet border-dotted rounded-xl `}>
-                                    Принять
-                                </div>
-                            </div> : null
+                            <ExpertMode
+                                factorEvals={factorEvals}
+                                factorS={getFactorById(factorConnectionData[currentFСEditor].start)}
+                                currentEval={currentEval}
+                                setCurrentEval={setCurrentEval}
+                                setEval={setEval}
+                            />
+                            : null
                         }
                         {factorConnectionData[currentFСEditor].fcEval.length ?
                             <>
---
+++ components/editor/modes/expertMode.tsx
@@ -0,0 +1,96 @@
+import { useEffect, useState, useRef } from "react"
+import axios from 'axios'
+import backendAxios from '../../network/backend'
+
+
+const ExpertMode = ({ factorEvals, factorS, currentEval, setCurrentEval, setEval }) => {
+    const [grouping, setGrouping] = useState(false);
+
+    useEffect(() => {
+        if (grouping) {
+            setCurrentEval(factorEvals.find(fe => fe.id === factorS.id).eLabels.map((fe) => 1))
+        }
+        else {
+            setCurrentEval([1])
+        }
+    }, [grouping])
+    return (
+        <>
+            <div className="text-lg mt-4 font-bold mb-1">Ручная оценка факторов</div>
+            {factorEvals.find(fe => fe.id === factorS.id)?.labels ?
+                <div className="flex items-center mt-2 ">
+                    <div
+                        onClick={() => {
+                            setGrouping(!grouping)
+
+                        }}
+                        className={`h-8 w-8 mr-1 border-dotted border-2 border-violet-border border-dotted rounded-md cursor-pointer ${grouping ? "bg-red-500" : ""}`}></div>
+                    <span>Разделить по термам</span>
+                </div>
+                : null}
+            <div className="flex-col mt-4">
+                {grouping ?
+                    <>
+                        {
+                            <>
+                                <div className="text-lg mt-4 font-bold mb-1">Ввести оценку</div>
+                                {
+                                    factorEvals.find(fe => fe.id === factorS.id).eLabels.map((fe, fi) =>
+                                        <>
+                                            <div>{fe}</div>
+                                            <input
+                                                value={currentEval[fi]}
+                                                onChange={(e) => {
+                                                    setCurrentEval(currentEval.map((ce, ci) => {
+                                                        if (ci === fi) {
+                                                            return e.target.value
+                                                        }
+                                                        return ce
+                                                    }))
+
+                                                }}
+                                                style={{
+                                                    outline: "none",
+                                                }}
+                                                className={`text-xl p-1 w-20 border-2 border-violet-border rounded-xl bg-violet`}
+                                            />
+                                        </>)
+
+                                }
+                            </>
+                        }
+                    </> :
+                    <>
+                        <div className="text-lg mt-4 font-bold mb-1">Ввести оценку</div>
+                        <input
+                            value={currentEval[0]}
+                            onChange={(e) => {
+                                setCurrentEval(currentEval.map((ce, ci) => {
+                                    if (ci === 0) {
+                                        return parseFloat(e.target.value)
+                                    }
+                                    return ce
+                                }))
+
+                            }}
+                            style={{
+                                outline: "none",
+                            }}
+                            className={`text-xl p-1 w-20 border-2 border-violet-border rounded-xl bg-violet`}
+                        />
+                    </>}
+
+                <div
+                    onClick={() => {
+                        setEval(currentEval.map(ce => Math.min(1, parseFloat(ce))))
+                    }}
+                    className={`h-16 mt-2  w-20 justify-center relative noselect z-40 transition-all duration-300 items-center flex w-full cursor-pointer text-white font-medium bg-violet-border border-2 border-violet border-dotted rounded-xl `}>
+                    Принять
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default ExpertMode 
